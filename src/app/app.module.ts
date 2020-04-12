5a496fee7462850133b2c8544b9e5b4fd4a4591b
New Name <New Email>
2020-04-11 17:42:19 -0700

    add module updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextboxComponent } from './utilities/input-textbox/input-textbox.component';
 import { TestComponent } from './utilities/test/test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRippleModule, MatLineModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,13 @@ import { TestComponent } from './utilities/test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
